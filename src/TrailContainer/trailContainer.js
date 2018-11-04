696dc7c6331b94f0958ac831adfc8fa54962250d
James <tytechortz@gmail.com>
2018-11-04 16:31:34 -0700

    finished create componet

--- src/TrailContainer/trailContainer.js
+++ src/TrailContainer/trailContainer.js
@@ -12,8 +12,28 @@ class TrailContainer extends Component {
     addTrail = async (trail, e) => {
         e.preventDefault();
         console.log(trail);
+
+        try {
+            const addedTrail = await fetch('http://localhost:9000/api/v1/trails', {
+                method: 'POST',
+                body: JSON.stringify(trail),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            const parsedResponse = await addedTrail.json();
+            console.log(parsedResponse, ' this is response')
+
+            this.setState({trails: [...this.state.trails, parsedResponse.data]})
+
+
+        } catch(err){
+            console.log(err)
+        }
     }
     render(){
+        console.log(this.state)
         return (
             <div>
                 <AddTrail addTrail={this.addTrail} />
