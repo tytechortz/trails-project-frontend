922be22175fac8725571da3b82bab9b0810e0831
James <tytechortz@gmail.com>
2018-11-04 18:36:38 -0700

    small change to list output

--- src/TrailList/trailList.js
+++ src/TrailList/trailList.js
@@ -4,19 +4,19 @@ const Trails = (props) => {
     const trails = props.trails.map((trail, i) => {
         return (
             <li key={trail._id}>
-                <h3>{trail.name}</h3>
-                <h5>{trail.location}</h5>
-                <h5>{trail.description}</h5>
-                <h5>{trail.rating}</h5>
+                <h3>Trail: {trail.name}</h3>
+                <h5>Location: {trail.location}</h5>
+                <h5>Description: {trail.description}</h5>
+                <h5>Rating: {trail.rating}</h5>
             </li>
         )
     })
     return (
         <div>
             <h1>Trails</h1>
-            <div>
-            {trails}
-            </div>
+            <ul>
+                {trails}
+            </ul>
         </div>
     )
 }
